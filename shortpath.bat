a5815ff477f795fd61e42bc8c2e76739ea096489
tigros <straightupandup@yahoo.com>
2017-01-31 19:31:18 -0500

    add local path

--- shortpath.bat
+++ shortpath.bat
@@ -16,6 +16,10 @@ reg add %pkey% /f /v Path /t REG_SZ /d "%tpath%"
 rem Broadcast the change
 powershell -command "& {$md=\"[DllImport(`\"user32.dll\"\",SetLastError=true,CharSet=CharSet.Auto)]public static extern IntPtr SendMessageTimeout(IntPtr hWnd,uint Msg,UIntPtr wParam,string lParam,uint fuFlags,uint uTimeout,out UIntPtr lpdwResult);\"; $sm=Add-Type -MemberDefinition $md -Name NativeMethods -Namespace Win32 -PassThru;$result=[uintptr]::zero;$sm::SendMessageTimeout(0xffff,0x001A,[uintptr]::Zero,\"Environment\",2,5000,[ref]$result)}"
 
+call :shortenlocalpath
+echo %tpath%
+endlocal && set path=%tpath%
+
 exit /b
 
 :shortenpath
@@ -37,6 +41,23 @@ exit /b
 
     exit /b
 
+:shortenlocalpath
+    set tpath=
+    set semi=
+
+    set mypath=%path%
+    set mypath=!mypath: =æ!     &rem Alt-145
+    set mypath=!mypath:^)=Æ!    &rem Alt-146
+
+    for %%N in (!mypath!) do (
+        set afolder=%%N
+        set afolder=!afolder:æ= !
+        set afolder=!afolder:Æ=^)!
+        call :shorten "!afolder!"
+    )
+
+    exit /b
+    
 :shorten
     set tpath=%tpath%%semi%%~s1
     set semi=;
